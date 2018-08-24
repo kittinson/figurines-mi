eebe4dee4a9554c2f8480c23674e45898084ce39
Ekaterina Gorbacheva <ekagorbacheva@mail.ru>
2018-08-24 23:22:16 +0800

    form fixes

--- source/send_form_email.php
+++ source/send_form_email.php
@@ -17,42 +17,36 @@ if(isset($_POST['email'])) {
 
 
     // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['phone']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['country']) ||
-        !isset($_POST['city']) ||
-        !isset($_POST['address']) ||
-        !isset($_POST['contact_type']) ||
-        !isset($_POST['instagram']) ||
-        !isset($_POST['telegram'])) {
+    if( !isset($_POST['contact_type']) ||
+        !isset($_POST['instagram'])
+        ) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');
     }
 
 
 
-    $name = $_POST['name']; // required
-    $phone = $_POST['phone']; // not required
-    $email = $_POST['email']; // required
-    $country = $_POST['country']; // required
-    $city = $_POST['city']; // required
-    $address = $_POST['address']; // required
+    // $name = $_POST['name']; // required
+    // $phone = $_POST['phone']; // not required
+    // $email = $_POST['email']; // required
+    // $country = $_POST['country']; // required
+    // $city = $_POST['city']; // required
+    // $address = $_POST['address']; // required
     $contact_type = $_POST['contact_type']; // not required
     $instagram = $_POST['instagram']; // required
-    $telegram = $_POST['telegram']; // required
+    // $telegram = $_POST['telegram']; // required
 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
-  if(!preg_match($email_exp,$email)) {
-    $error_message .= 'Похоже, Email неверный. Введи настоящий. ;)<br />';
-  }
+  // if(!preg_match($email_exp,$email)) {
+  //   $error_message .= 'Похоже, Email неверный. Введи настоящий. ;)<br />';
+  // }
 
     $string_exp = "/^[A-Za-z .'-]+$/";
 
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'Твоё полное имя нужно нам, чтобы оформить доставку. Пожалуйста, введи реальные данные.<br />';
-  }
+  // if(!preg_match($string_exp,$name)) {
+  //   $error_message .= 'Твоё полное имя нужно нам, чтобы оформить доставку. Пожалуйста, введи реальные данные.<br />';
+  // }
 
 
   if(strlen($error_message) > 0) {
@@ -69,15 +63,15 @@ if(isset($_POST['email'])) {
 
 
 
-    $email_message .= "Имя: ".clean_string($name)."\n";
-    $email_message .= "Телефон: ".clean_string($phone)."\n";
-    $email_message .= "Email: ".clean_string($email)."\n";
-    $email_message .= "Страна: ".clean_string($country)."\n";
-    $email_message .= "Город: ".clean_string($city)."\n";
-    $email_message .= "Адрес: ".clean_string($address)."\n";
-    $email_message .= "Контакт: ".clean_string($contact_type)."\n";
-    $email_message .= "Инстаграм: ".clean_string($instagram)."\n";
-    $email_message .= "Телеграм: ".clean_string($telegram)."\n";
+    // $email_message .= "Имя: ".clean_string($name)."\n";
+    // $email_message .= "Телефон: ".clean_string($phone)."\n";
+    // $email_message .= "Email: ".clean_string($email)."\n";
+    // $email_message .= "Страна: ".clean_string($country)."\n";
+    // $email_message .= "Город: ".clean_string($city)."\n";
+    // $email_message .= "Адрес: ".clean_string($address)."\n";
+    $email_message .= "Вариант: ".clean_string($contact_type)."\n";
+    $email_message .= "Контакт: ".clean_string($instagram)."\n";
+    // $email_message .= "Телеграм: ".clean_string($telegram)."\n";
 
 
 // create email headers
